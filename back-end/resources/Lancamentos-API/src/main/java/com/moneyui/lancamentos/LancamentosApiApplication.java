a3a2f8c49ad3f056cbdf6c73909b3f2c82bcbee4
Gabriel Alan <alang.von@gmail.com>
2021-01-12 22:59:45 -0300

    Lancamentos-API has been released

---
+++ back-end/resources/Lancamentos-API/src/main/java/com/moneyui/lancamentos/LancamentosApiApplication.java
@@ -0,0 +1,13 @@
+package com.moneyui.lancamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LancamentosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LancamentosApiApplication.class, args);
+	}
+
+}
